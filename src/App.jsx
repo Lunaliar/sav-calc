a3e1c2d607a26a490145c2f39c73d6b2c58fda48
sav <savcostabile@gmail.com>
2023-02-03 12:25:02 -0500

    initer

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import calculate from "calculate-string";
+import { useState } from "react";
+
+function App() {
+	const [calc, setCalc] = useState("");
+	const [result, setResult] = useState("");
+
+	const operators = ["/", "*", "+", "-", "CE", "x²", "√x", "←"];
+	const digits = [];
+	const getDigits = () => {
+		for (let i = 9; i >= 0; i--) {
+			i === 0 ? digits.push(".", i, "=") : digits.push(i);
+		}
+	};
+	getDigits();
+
+	const calcAll = () => {
+		setCalc(calculate(calc.toString()));
+		setResult("");
+	};
+
+	const deleteLast = () => calc !== "" && setCalc((prev) => prev.slice(0, -1));
+
+	const clearAll = () => {
+		setCalc("");
+		setResult("");
+	};
+
+	const square = () =>
+		setCalc((prev) => Math.pow(calculate(prev.toString()), 2).toString());
+
+	const root = () => setCalc((prev) => Math.sqrt(calculate(prev)).toString());
+
+	const updateCalc = (value) => {
+		const allOperators = [...operators, "."];
+		if (value === "=") return calcAll();
+		else if (value === "←") return deleteLast();
+		else if (value === "CE") return clearAll();
+		else if (value === "x²") {
+			if (allOperators.includes(calc.slice(-1))) return;
+			return square();
+		} else if (value === "√x") {
+			if (allOperators.includes(calc.slice(-1))) return;
+			return root();
+		} else if (value === "-") {
+			if (allOperators.includes(calc.slice(-1))) return;
+			setCalc(calc + value);
+		} else if (
+			(allOperators.includes(value) && calc === "") ||
+			(allOperators.includes(value) && allOperators.includes(calc.slice(-1)))
+		) {
+			return;
+		}
+		setCalc(calc + value);
+		if (!allOperators.includes(value)) {
+			setResult(calculate((calc + value).toString()));
+		}
+	};
+	return (
+		<div className="App">
+			<div className="calculator">
+				<div className="display">
+					{result ? <span>({result})</span> : ""}
+					&nbsp;
+					{calc || 0}
+				</div>
+				<div className="operators">
+					{operators.map((operator) => (
+						<button key={operator} onClick={() => updateCalc(operator)}>
+							{operator}
+						</button>
+					))}
+				</div>
+
+				<div className="digits">
+					{digits.map((digit) => (
+						<button key={digit} onClick={() => updateCalc(digit)}>
+							{digit}
+						</button>
+					))}
+				</div>
+			</div>
+			<p className="credit">
+				<a href="https://savcodes.dev">Sav Costabile</a>&nbsp;© 2022
+			</p>
+		</div>
+	);
+}
+
+export default App;
